8559e8f0a47c000b3e7818b60ff74a6826aab6db
aryam <arya.mishra@bonamisoftware.com>
2025-03-03 11:44:59 +0530

    Changes in Manual Redis

--- redis/src/main/java/com/example/manualredis/ManualRedisApplication.java
+++ redis/src/main/java/com/example/manualredis/ManualRedisApplication.java
@@ -1,12 +1,30 @@
 package com.example.manualredis;
 
+import com.example.manualredis.model.Student;
+import com.example.manualredis.service.IStudentDao;
 import com.example.redis.RedisApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManualRedisApplication {
+public class ManualRedisApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
     }
-}
+    @Autowired
+    private IStudentDao studentDao;
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+            studentDao.addStudent(new Student(101,"Arya Mishra",300.20));
+            studentDao.addStudent(new Student(102,"Kunal Mishra",300.20));
+            studentDao.addStudent(new Student(103,"Love Mishra",300.20));
+            studentDao.addStudent(new Student(104,"Avika Mishra",300.20));
+
+            studentDao.getAllStudent().forEach((k,v)-> System.out.println(k+ "--" +v));
+        }
+    }
--- redis/src/main/java/com/example/manualredis/service/impl/IStudentDaoImpl.java
+++ redis/src/main/java/com/example/manualredis/service/impl/IStudentDaoImpl.java
@@ -2,6 +2,7 @@ package com.example.manualredis.service.impl;
 
 import com.example.manualredis.model.Student;
 import com.example.manualredis.service.IStudentDao;
+import jakarta.annotation.Resource;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.stereotype.Service;
 
@@ -10,31 +11,37 @@ import java.util.Map;
 @Service
 public class IStudentDaoImpl implements IStudentDao {
 
+    private final String KEY="STUDENT";
     //reference type,key type,value type
+    @Resource(name="rt")
     private HashOperations<String,Integer,Student> hashOperations;
 
     @Override
     public void addStudent(Student student) {
 
+        hashOperations.putIfAbsent(KEY,student.getStId(),student);
     }
 
     @Override
     public void modifyStudent(Student student) {
-
+         hashOperations.put(KEY,student.getStId(),student);
     }
 
     @Override
     public Student getOneStudent(Integer id) {
-        return null;
+
+        return hashOperations.get(KEY,id);
     }
 
     @Override
     public Map<Integer, Student> getAllStudent() {
-        return Map.of();
+        return hashOperations.entries(KEY);
     }
 
     @Override
     public void removeStudent(Integer id) {
 
+        //hash ref,key
+            hashOperations.delete(KEY,id);
     }
 }
